58f2a3269bc973b6aa6de565b744ea8230f224b1
Rohit Amarnath <rohit.amarnath@full360.com>
2014-05-16 17:31:29 -0400

    volt enterprise and initial commit

---
+++ voltdb-ent.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class VoltdbBinary < Formula
+  version '4.3'
+  homepage 'https://github.com/VoltDB/voltdb'
+
+  if OS.mac?
+    url 'http://voltdb.com/downloads/technologies/server/MAC-voltdb-ent-4.3.tar.gz'
+    sha256 'b6f566af9e4758c93fcfe0f222377405edb093ed'
+  elsif OS.linux?
+    url 'http://voltdb.com/downloads/technologies/server/LINUX-voltdb-ent-4.3.tar.gz'
+    sha1 '281e6541d0a98e737ec17576d5ae03fef36ff46d'
+  end
+
+  def install
+
+    inreplace Dir["bin/*"] - ["bin/voltadmin"],
+      /VOLTDB_LIB=\$VOLTDB_HOME\/lib/, 'VOLTDB_LIB=$VOLTDB_HOME/lib/voltdb'
+
+    (lib/'voltdb').install Dir['lib/*']
+    lib.install_symlink lib/"voltdb/python"
+    prefix.install 'bin', 'tools', 'voltdb', 'version.txt', 'doc'
+  end
+end
