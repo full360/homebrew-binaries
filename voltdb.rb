367570688c519e4100bff76c6965b29d2d554428
Rohit Amarnath <rohit.amarnath@full360.com>
2016-07-22 13:28:00 -0400

    fix sha and inreplace

--- voltdb.rb
+++ voltdb.rb
@@ -6,14 +6,14 @@ class Voltdb < Formula
 
   if OS.mac?
     url 'http://voltdb.com/downloads/technologies/server/MAC-voltdb-ent-6.4.tar.gz'
-    sha256 '977f81869a217381b3fd6e791c384f13e170f60d95bd7c7baf6bb10a5968804d'
+    sha256 'd4b9bacb498e53d91c42366beee541d125dd0f47ad54f2041406dd9e9628aea5'
   elsif OS.linux?
     url 'http://voltdb.com/downloads/technologies/server/LINUX-voltdb-ent-6.4.tar.gz'
   end
 
   def install
 
-    inreplace Dir["bin/*"] - ["bin/voltadmin"] - ["bin/dragent"] - ["bin/voltdb"] ,
+    inreplace Dir["bin/*"] - ["bin/voltadmin", "bin/dragent", "bin/voltdb", "bin/rabbitmqloader", "bin/voltdeploy", "bin/voltenv"] ,
       /VOLTDB_LIB=\$VOLTDB_HOME\/lib/, 'VOLTDB_LIB=$VOLTDB_HOME/lib/voltdb'
 
     (lib/'voltdb').install Dir['lib/*']
